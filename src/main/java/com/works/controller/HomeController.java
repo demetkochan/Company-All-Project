a3d25695a340e53bca70aa1b2e051c2eb358e902
İlayda Güler <ilydagulr@gmail.com>
2021-10-08 22:09:47 +0300

    update HomeController

--- src/main/java/com/works/controller/HomeController.java
+++ src/main/java/com/works/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.works.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dashboard")
 public class HomeController {
 
-    @GetMapping("/dashboard")
+    @GetMapping("")
     public String vendor(Model model){
 
         return "home";
