51608e07e951203a705d1d483c08b308965c03ec
demetkochan <demetkochan@hotmail.com>
2021-10-08 16:46:57 +0300

    inital project

---
+++ src/main/java/com/works/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.works.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String vendor(Model model){
+
+        return "home";
+    }
+}
