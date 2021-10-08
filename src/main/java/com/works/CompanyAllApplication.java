c89187c6b0ee7b0b67379fca0ba9386734fa20dc
İlayda Güler <ilydagulr@gmail.com>
2021-10-08 17:22:12 +0300

    add Project

---
+++ src/main/java/com/works/CompanyAllApplication.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyAllApplication.class, args);
+    }
+
+}
