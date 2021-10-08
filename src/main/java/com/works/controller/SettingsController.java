c9eefeacf5d9fde583b91e6cf2bc6b654d02efc3
Emine Şahsanem Demirel <sahsanem.demirel@gmail.com>
2021-10-08 23:40:11 +0300

    Project Source Code

---
+++ src/main/java/com/works/controller/SettingsController.java
@@ -0,0 +1,14 @@
+package com.works.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/settings")
+public class SettingsController {
+    @GetMapping("")
+    public String settings(){
+        return "settings";
+    }
+}
