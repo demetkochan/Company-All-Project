a2840c11af57db0ef6b1881c01449204d267c4f1
İlayda Güler <ilydagulr@gmail.com>
2021-10-09 13:04:41 +0300

    Html update

--- src/main/java/com/works/controller/CustomerController.java
+++ src/main/java/com/works/controller/CustomerController.java
@@ -3,12 +3,14 @@ package com.works.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
 
-    @GetMapping("/customer")
+    @GetMapping(" ")
     public String customer(Model model){
         return "customer";
     }
