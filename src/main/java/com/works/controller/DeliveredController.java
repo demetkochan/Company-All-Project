076a9794ef3795df05a86bd88d549b0ed50e652a
demetkochan <demetkochan@hotmail.com>
2021-10-09 23:03:28 +0300

    order-delivered add

---
+++ src/main/java/com/works/controller/DeliveredController.java
@@ -0,0 +1,16 @@
+package com.works.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/delivered")
+public class DeliveredController {
+
+    @GetMapping("")
+    public String delivered(Model model){
+        return "delivered";
+    }
+}
---
+++ src/main/java/com/works/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.works.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("")
+    public String order(Model model){
+        return "order";
+    }
+}
