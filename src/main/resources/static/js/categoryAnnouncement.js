d18d2787746d9f20d868511e0a59d31c49d72075
Emine Şahsanem Demirel <sahsanem.demirel@gmail.com>
2021-10-10 22:40:41 +0300

    Update Category

--- src/main/resources/static/js/categoryAnnouncement.js
+++ src/main/resources/static/js/categoryAnnouncement.js
@@ -136,3 +136,211 @@ function fncNewsCategoryUpdate( i ) {
     console.log(select_id)
     $("#news_categoryName").val(item.news_categoryName)
 }
+
+$('#addProductCategory').submit((event) => {
+    console.log("Tıklandı.")
+    event.preventDefault();
+
+    const product_categoryName = $("#product_categoryName").val()
+
+    const obj = {
+        product_categoryName: product_categoryName,
+    }
+
+    $.ajax({
+        url: './category_mvc/productAdd',
+        type: 'POST',
+        data: JSON.stringify(obj),
+        dataType: 'json',
+        contentType : 'application/json; charset=utf-8',
+        success: function (data) {
+            if (data) {
+                console.log(data)
+                $("#product_categoryName").val(" ")
+            } else {
+                console.log("Veri dönmedi.")
+            }
+        },
+        error: function (err) {
+            console.log(err)
+            alert("İşlem işlemi sırısında bir hata oluştu!");
+        }
+    })
+})
+//-----------------------------------------Product Category Add Finish--------------------------------------------------------//
+
+//--------------------------------------------Product Category list  --------------------------------------------//
+function allProductCategoryResult(){
+    $.ajax({
+        url: './category_mvc/productList',
+        type: 'GET',
+        contentType : 'application/json; charset=utf-8',
+        success: function (data) {
+            console.log(data)
+            createRo(data)
+        },
+        error: function (err) {
+            console.log(err)
+            alert("İşlem işlemi sırısında bir hata oluştu!");
+        }
+    })
+}
+allProductCategoryResult()
+
+//-------------------------------------------- News Table  --------------------------------------------//
+let globalAr = []
+function createRo(data){
+    let html = ``
+    for (let i = 0; i < data.length; i++) {
+        globalAr = data
+        const itm = data[i]
+        html += `<tr>
+          <th scope="row">${itm.id}</th>
+          <td>${itm.product_categoryName}</td>
+           <td class="text-right" >
+               <div class="btn-group" role="group">
+                    <button onclick="fncProductCategoryUpdate(`+i+`)" type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#categoryNewsUpdateModal">Güncelle</button>
+                    <button onclick="fncProductCategoryDelete(${itm.id})" type="button" class="btn btn-outline-danger ">Sil</button>
+               </div>
+          </td>
+
+        </tr>`
+    }
+    $("#tableProductRow").html(html)
+}
+//-------------------------------------------- News Delete Function --------------------------------------------//
+function fncProductCategoryDelete(id){
+    let answer = confirm("Silmek istediğinize emin misiniz?")
+    if(answer){
+
+        $.ajax({
+            url:"./category_mvc/productDelete/"+id,
+            type:"delete",
+            dataType: 'text',
+            success: function (data){
+                console.log(typeof data)
+                if( data != "0" ){
+                    alert("Silme İşlemi Başarılı!")
+                    allProductCategoryResult()
+                }else {
+                    alert("Silme sırasında bir hata oluştu.")
+                }
+            },
+            error: function (err){
+                console.log(err)
+            }
+        })
+    }
+}
+//-------------------------------------------- Product Update Function --------------------------------------------//
+function fncProductCategoryUpdate( i ) {
+
+    const item = globalArr[i];
+    select_id = item.id
+    console.log(select_id)
+    $("#product_categoryName").val(item.product_categoryName)
+}
+//--------------------------Gallery-------------------------------------------------//
+$('#addGalleryCategory').submit((event) => {
+    console.log("Tıklandı.")
+    event.preventDefault();
+
+    const gallery_categoryName = $("#gallery_categoryName").val()
+
+    const obj = {
+        gallery_categoryName: gallery_categoryName,
+    }
+
+    $.ajax({
+        url: './category_mvc/galleryAdd',
+        type: 'POST',
+        data: JSON.stringify(obj),
+        dataType: 'json',
+        contentType : 'application/json; charset=utf-8',
+        success: function (data) {
+            if (data) {
+                console.log(data)
+                $("#news_categoryName").val(" ")
+            } else {
+                console.log("Veri dönmedi.")
+            }
+        },
+        error: function (err) {
+            console.log(err)
+            alert("İşlem işlemi sırısında bir hata oluştu!");
+        }
+    })
+})
+//-----------------------------------------News Category Add Finish--------------------------------------------------------//
+
+//-------------------------------------------- News Category list  --------------------------------------------//
+function allGalleryCategoryResult(){
+    $.ajax({
+        url: './category_mvc/galleryList',
+        type: 'GET',
+        contentType : 'application/json; charset=utf-8',
+        success: function (data) {
+            console.log(data)
+            cRow(data)
+        },
+        error: function (err) {
+            console.log(err)
+            alert("İşlem işlemi sırısında bir hata oluştu!");
+        }
+    })
+}
+allGalleryCategoryResult()
+
+//-------------------------------------------- News Table  --------------------------------------------//
+let globalarr = []
+function cRow(data){
+    let html = ``
+    for (let i = 0; i < data.length; i++) {
+        globalarr = data
+        const itm = data[i]
+        html += `<tr>
+          <th scope="row">${itm.id}</th>
+          <td>${itm.gallery_categoryName}</td>
+           <td class="text-right" >
+               <div class="btn-group" role="group">
+                    <button onclick="fncGalleryCategoryUpdate(`+i+`)" type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#categoryNewsUpdateModal">Güncelle</button>
+                    <button onclick="fncGalleryCategoryDelete(${itm.id})" type="button" class="btn btn-outline-danger ">Sil</button>
+               </div>
+          </td>
+
+        </tr>`
+    }
+    $("#tableGalleryRow").html(html)
+}
+//-------------------------------------------- News Delete Function --------------------------------------------//
+function fncGalleryCategoryDelete(id){
+    let answer = confirm("Silmek istediğinize emin misiniz?")
+    if(answer){
+
+        $.ajax({
+            url:"./category_mvc/galleryDelete/"+id,
+            type:"delete",
+            dataType: 'text',
+            success: function (data){
+                console.log(typeof data)
+                if( data != "0" ){
+                    alert("Silme İşlemi Başarılı!")
+                    allGalleryCategoryResult()
+                }else {
+                    alert("Silme sırasında bir hata oluştu.")
+                }
+            },
+            error: function (err){
+                console.log(err)
+            }
+        })
+    }
+}
+//-------------------------------------------- News Update Function --------------------------------------------//
+function fncGalleryCategoryUpdate( i ) {
+
+    const item = globalarr[i];
+    select_id = item.id
+    console.log(select_id)
+    $("#gallery_categoryName").val(item.gallery_categoryName)
+}
