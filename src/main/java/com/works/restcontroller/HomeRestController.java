898527c16fc0272492c4a4181fc5062f59802df1
demetkochan <demetkochan@hotmail.com>
2021-10-13 00:30:31 +0300

    add home rest

---
+++ src/main/java/com/works/restcontroller/HomeRestController.java
@@ -0,0 +1,56 @@
+package com.works.restcontroller;
+
+
+import com.works.dto.HomeDto;
+import com.works.util.ERest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/home_rest")
+public class HomeRestController {
+
+    final HomeDto homeDto;
+
+    public HomeRestController(HomeDto homeDto) {
+        this.homeDto = homeDto;
+    }
+
+    //Toplam İçerik
+    @GetMapping("/contentCount")
+    public Map<ERest,Object> contentCount(){
+        return homeDto.totalContent();
+    }
+
+    //Toplam Haber
+    @GetMapping("/newsCount")
+    public Map<ERest,Object> newsCount(){
+        return homeDto.totalNews();
+    }
+    //Toplam Haber Aktif
+    @GetMapping("/newsActiveCount")
+    public Map<ERest,Object> newsActiveCount(){
+        return homeDto.totalNewsActive();
+    }
+    //Toplam Haber Pasif
+    @GetMapping("/newsPassiveCount")
+    public Map<ERest,Object> newsPassiveCount(){
+        return homeDto.totalNewsPassive();
+    }
+
+    //Toplam Duyuru
+    @GetMapping("/announcementCount")
+    public Map<ERest,Object> announcementCount(){
+        return homeDto.totalAnnouncement();
+    }
+
+    //Toplam Ürün
+    @GetMapping("/productCount")
+    public Map<ERest,Object> productCount(){
+        return homeDto.totalProduct();
+    }
+
+}
