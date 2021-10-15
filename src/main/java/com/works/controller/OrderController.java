c5ee7cda43895021af15e58b23757d3bd78f2491
Emine Şahsanem Demirel <sahsanem.demirel@gmail.com>
2021-10-15 23:24:38 +0300

    Update

--- src/main/java/com/works/controller/OrderController.java
+++ src/main/java/com/works/controller/OrderController.java
@@ -1,16 +1,26 @@
 package com.works.controller;
 
+import com.works.services.UtilServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/order_mvc")
 public class OrderController {
+    final UtilServices uService;
+
+    public OrderController(UtilServices uService) {
+        this.uService = uService;
+    }
 
     @GetMapping("")
     public String order(Model model){
+        model.addAttribute("customer",uService.customerList());
+        model.addAttribute("product",uService.productList());
         return "order";
     }
 }
