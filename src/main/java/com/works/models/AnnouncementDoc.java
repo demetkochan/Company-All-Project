a2eb2df68d4af58fdc8004e11a9dac41d3db724b
Emine Şahsanem Demirel <sahsanem.demirel@gmail.com>
2021-10-20 19:43:55 +0300

    Announcement Elasticsearch Code

--- src/main/java/com/works/models/AnnouncementDoc.java
+++ src/main/java/com/works/models/AnnouncementDoc.java
@@ -1,18 +1,11 @@
 package com.works.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.util.Date;
 import java.util.UUID;
 
 @Document(indexName = "announcementdoc")
@@ -28,10 +21,10 @@ public class AnnouncementDoc {
     @Field(type=FieldType.Text)
     private String announcement_detail_desc;
 
-    @Min(value = 1)
+
     @Field(type=FieldType.Text)
     private String  announcement_status;
 
-    @Field(type=FieldType.Date)
-    private Date announcement_date;
+    @Field(type=FieldType.Text)
+    private String announcement_date;
 }
